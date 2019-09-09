41a4c6602b73dedce5461953b9da09c6bb045982
Nikhil Patel <nikhil.patel@bacancytechnology.com>
2019-09-09 17:15:57 +0530

    integrate signature upload with signpopup modal

--- src/popupModals/SignModal.js
+++ src/popupModals/SignModal.js
@@ -8,6 +8,7 @@ import {
 import SignatureCanvas from 'react-signature-canvas';
 import './signModal.css'
 import classnames from 'classnames';
+import SignatureUpload from "../utils/dragNdropFileupload";
 import { toastError } from "../NotificationToast";
 
 class SignModal extends React.Component {
@@ -15,7 +16,7 @@ class SignModal extends React.Component {
 		super(props);
 		this.sigPad = {};
 		this.state = {
-			activeTab: '1',
+			activeTab: "1",
 			signImg: null,
 			isSignChange: false,
 		};
@@ -23,11 +24,14 @@ class SignModal extends React.Component {
 
 	componentDidUpdate = (prevProps) => {
 		if (prevProps.open !== this.props.open) {
+			console.log("etfsdfsdg"
+			)
 			this.setState({
 				isSignChange: !this.props.open,
 				signImg: this.props.signatureUrl,
 			});
 		}
+
 	}
 
 	onSetSign = () => {
@@ -61,8 +65,31 @@ class SignModal extends React.Component {
 		}
 	}
 
+	onFileDrop = (file) => {
+		const promise = new Promise((resolve, reject) => {
+			const reader = new FileReader()
+			reader.readAsDataURL(file)
+			reader.onload = () => {
+				if (!!reader.result) {
+					resolve(reader.result)
+				}
+				else {
+					reject(Error("Failed converting to base64"))
+				}
+			}
+		})
+		promise.then(result => {
+			this.setState({
+				isSignChange: true,
+				signImg: result,
+			})
+		}, err => {
+			toastError("Invalid Image")
+		})
+	}
+
 	render() {
-		const { signImg, isSignChange } = this.state;
+		const { signImg, isSignChange, activeTab } = this.state;
 		return (
 			<div>
 				<Modal size={"md"} isOpen={this.props.open}>
@@ -89,27 +116,23 @@ class SignModal extends React.Component {
 										<NavItem>
 											<NavLink
 												style={{ cursor: "pointer" }}
-												className={classnames({ active: this.state.activeTab === '1' })}
-												onClick={() => { this.toggleTabs('1'); }}
-											>
-												DRAW
-                      </NavLink>
+												className={classnames({ active: activeTab === "1" })}
+												onClick={() => { this.toggleTabs("1"); }}
+											>DRAW</NavLink>
 										</NavItem>
 										<NavItem>
 											<NavLink
 												style={{ cursor: "pointer" }}
-												className={classnames({ active: this.state.activeTab === '2' })}
-												onClick={() => { this.toggleTabs('2'); }}
-											>
-												UPLOAD
-                    	</NavLink>
+												className={classnames({ active: activeTab === "2" })}
+												onClick={() => { this.toggleTabs("2"); }}
+											>UPLOAD</NavLink>
 										</NavItem>
 									</Nav>
 								</Col>
 							</Row>
 							<Row>
 								<Col md={12} >
-									<TabContent activeTab={this.state.activeTab}>
+									<TabContent activeTab={activeTab}>
 										<TabPane tabId="1">
 											<div className={"sigContainer"}>
 												<SignatureCanvas ref={(ref) => { this.sigPad = ref }} penColor='black' canvasProps={{ className: "sigPad" }} clearOnResize={false} />
@@ -122,7 +145,7 @@ class SignModal extends React.Component {
 										<TabPane tabId="2">
 											<Row>
 												<Col sm="12">
-													<h4>Tab 2 Contents</h4>
+													<SignatureUpload onFileDrop={this.onFileDrop} showFileMeta={false} />
 												</Col>
 											</Row>
 										</TabPane>
@@ -141,33 +164,6 @@ class SignModal extends React.Component {
 								:
 								null
 							}
-							{/* <Row>
-										<Col md={12} >
-												<FormGroup>
-													<Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor"/>
-												</FormGroup>
-										</Col>
-								</Row> */}
-							{/* <Row>
-										<Col md={6}>
-												<FormGroup>
-												<Label for="exampleCity">City</Label>
-												<Input type="text" name="city" id="exampleCity"/>
-												</FormGroup>
-										</Col>
-										<Col md={4}>
-												<FormGroup>
-												<Label for="exampleState">State</Label>
-												<Input type="text" name="state" id="exampleState"/>
-												</FormGroup>
-										</Col>
-										<Col md={2}>
-												<FormGroup>
-												<Label for="exampleZip">Zip</Label>
-												<Input type="text" name="zip" id="exampleZip"/>
-												</FormGroup>  
-										</Col>
-								</Row> */}
 						</Form>
 					</ModalBody>
 					<ModalFooter>
